c4ead1382fc5971d2270d4ec0a26f815a8c787ba
Stefan <39063902+ReficGithub@users.noreply.github.com>
2023-09-23 11:49:18 +0200

    Update train.py

--- train.py
+++ train.py
@@ -24,7 +24,7 @@ Xval = Xval[:, :30 * 4].reshape(-1, 30, 4)
 input_shape = (Xtrain.shape[1], Xtrain.shape[2])
 output_shape = ytrain.shape[1]
 batch_size = 32
-epochs = 25
+epochs = 250
 
 def bouw_lstm_netwerk(input_shape, output_shape):
     model = Sequential()
